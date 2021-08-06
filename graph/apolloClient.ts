d159b6b7f70788d85911dbc08c496bc103ce81f3
Prescott Rynewicz <prescott@cameo.com>
2021-08-06 09:09:40 -0700

    Fix Env Variable for webite url.

--- graph/apolloClient.ts
+++ graph/apolloClient.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, from, HttpLink, InMemoryCache } from '@apollo/client';
 
 const domain =
-  process.env.WEBSITE_URL || `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  process.env.NEXT_PUBLIC_WEBSITE_URL ||
+  `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 
 const httpLink = new HttpLink({
   uri: `${domain}/api/graphql`,
